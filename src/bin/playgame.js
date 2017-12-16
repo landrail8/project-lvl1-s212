fb542fb09f600bdbbf21ec43e9402f753de33371
Alex <88888alexandr@gmail.com>
2017-12-16 12:42:07 +0300

    make interface for 4 games v0

--- src/bin/playgame.js
+++ src/bin/playgame.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync';
 import brain_calc from '../games/brain-calc';
 import brain_even from '../games/brain-even';
 import brain_gcd from '../games/brain-gcd';
+import brain_balance from '../games/brain-balance';
 
 
 const fs = require('fs');
@@ -35,12 +36,15 @@ const numberGame = readlineSync.question('Press number of the Game and play: ');
 
 switch (numberGame) {
   case '0': {
+    brain_balance();
+    break; }
+  case '1': {
     brain_calc();
     break; }
-  case '1':
+  case '2':
     brain_even();
     break;
-  case '2':
+  case '3':
     brain_gcd();
     break;
   default:
